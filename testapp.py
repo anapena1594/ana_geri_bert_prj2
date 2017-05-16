620c653137dee2b1d5fc1f9df35e55fb6fd98f05
gerijos <yjose@csumb.edu>
2017-05-15 22:30:45 -0700

    Update testapp.py

--- testapp.py
+++ testapp.py
@@ -1,4 +1,24 @@
+'''
+Title:testapp.py
+Abstract:This programs utilize python libraries and filters to create a webpage,
+	 this initialize the images that are being upload and they are display in the webpage.
+	 The webpage display six different filters with the same picture and the original image.
+	 Each time the user uploads an image it grabs the image and (GET & POST) make the picture 
+	 go through different funtions for each filter. Each filter is then directed to the webpage,
+	 the user has the option to right click on the image to save it.
+	 Author: Humberto Plaza, Ana Pena, Yeraldiny Jose Alonzo
 
+Ana: Worked on the red and blur filters and contributed to the code (py and html) section
+Humberto:Worked on the html page and figured how to display the images in six different 
+	 boxes as well as contributing with the filters functions.
+Yeraldiny: Worked on the upload section and contributed to the code (py and html) section
+
+GitHub Link: https://github.com/anapena1594/ana_geri_bert_prj2/blob/master/testapp.py
+
+Each member code that was done was copy and pasted into cloud 9 in order to push it to GitHub
+
+Date: May 5, 2017
+'''
 import os
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug import secure_filename 
@@ -16,20 +36,20 @@ from PIL import Image, ImageFilter
 
 app = Flask(__name__)#initialize the Flask application
 '''
-This route will show a form to perform an AJAX request, AJAX is a method that exhange
-#data with a server, and update parts of a web page wihout reloading the whole page.
-@app.route('/')
+Description:This route will show a form to perform an AJAX request, AJAX is a method that exhange
+	   #data with a server, and update parts of a web page wihout reloading the whole page.
+e@app.route('/')
 '''
 
 
 """
-The UPLOAD_FOLDER stores the uploaded images and the ALLOWED_EXTENSIONS
-are the file extensions that the user is able to upload
+Description:The UPLOAD_FOLDER stores the uploaded images and the ALLOWED_EXTENSIONS
+	    are the file extensions that the user is able to upload
 """
 
 UPLOAD_FOLDER = 'static/'  #This is the path to the upload directory
 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif']) 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif']) #
 
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
